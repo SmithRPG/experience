5cc694e3061354f7c892f425fc5dd2fca18b7420
John Smith <alvaroarribas1994@gmail.com>
2018-06-04 08:51:45 +0200

    Add files via upload

---
+++ es_extended/config.lua
@@ -0,0 +1,13 @@
+Config                          = {}
+Config.MaxPlayers               = 32
+Config.Accounts                 = { 'bank', 'black_money', 'experience' }
+-- Config.AccountLabels            = { bank = 'Banque', black_money = 'Argent Sale' } -- French
+Config.AccountLabels            = { bank = 'Banco', black_money = 'Dinero Sucio', experience = 'Experiencia' } -- English
+Config.PaycheckInterval         = 20 * 60000
+Config.ShowDotAbovePlayer       = false
+Config.DisableWantedLevel       = true
+Config.RemoveInventoryItemDelay = 30000
+Config.EnableWeaponPickup       = false
+Config.Locale                   = 'en'
+Config.EnableDebug              = true
+
